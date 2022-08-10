4780f1825d122a64659ff73c2d32bd48cef9e055
pogorelof <v.pogorelof@gmail.com>
2022-08-10 17:31:59 +0600

    some updates

--- dags/currency.py
+++ dags/currency.py
@@ -1,82 +1,69 @@
 from urllib import response
-import psycopg2
 import requests
 import json 
 from datetime import timedelta, datetime
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.postgres_operator import PostgresOperator
 
 args = {
     "owner":"pogorelov",
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
     "start_date": datetime(2022,1,1),
+    "provide_context": True
 }
 
-def create_table():
-    connect = psycopg2.connect(
-        database = "airflow",
-        user = "airflow",
-        password = "airflow",
-        host = "postgres",
-        port = "5432"
-    )
-    cursor = connect.cursor()
-    cursor.execute(
-        'CREATE TABLE if not exists currency('+
-        'id serial,'+
-        'json json,'+
-        'date timestamp with time zone'+
-        ');'
-    )
-    connect.commit()  
-    connect.close()
-
-def get_currency():
-    connect = psycopg2.connect(
-        database = "airflow",
-        user = "airflow",
-        password = "airflow",
-        host = "postgres",
-        port = "5432"
-    )
-    cursor = connect.cursor()
-
+def extract(**kwargs):
+    ti = kwargs['ti']
     #получение всех валют
     url = 'https://api.exchangerate.host/latest'
     response = requests.get(url)
-    json_from_api = response.json()
+    json_from_api = response.json()['rates']
+    ti.xcom_push(key = 'json', value = json_from_api)
 
+def transform(**kwargs):
+    ti = kwargs['ti']
+    json_from_api = ti.xcom_pull(key = 'json', task_ids = 'extract_data')
     #фильтрация
-    json_from_api = json_from_api['rates']
     dictionary = {
         'KZT': json_from_api['KZT'],
         'RUB': json_from_api['RUB'],
         'BTC': json_from_api['BTC']
     }
     json_to_db = json.dumps(dictionary, indent=4)
-
-    #запрос на добавление новой строки в бд
-    cursor.execute(
-        'INSERT INTO currency(json, date) VALUES (\'' + json_to_db + '\', current_timestamp);'
-    )
-    connect.commit()
-    connect.close() 
+    ti.xcom_push(key = 'json_db', value = json_to_db)
 
 
 with DAG(
     dag_id = "currency",
-    schedule_interval = "*/1 * * * *",
+    schedule_interval = "20 10-21 * * MON-FRI",
     default_args = args,
     catchup=False
     ) as f:
-    create_table = PythonOperator(
-        task_id = "CreateTable",
-        python_callable = create_table,
-    )
-    get_currency = PythonOperator(
-        task_id = "GetCurrency",
-        python_callable = get_currency,
-    )
-
-    create_table >> get_currency
+    extract = PythonOperator(task_id = 'extract_data', python_callable = extract,)
+    transform = PythonOperator(task_id = 'transform_data', python_callable = transform,)
+    create_table = PostgresOperator(
+        task_id = 'create_table',
+        postgres_conn_id = 'datapg',
+        sql = 
+        """
+                CREATE TABLE if not exists currency(
+                id serial,
+                json json,
+                date timestamp with time zone
+                );
+        """,
+        )
+    insert_in_table = PostgresOperator(
+        task_id = 'insert_in_table',
+        postgres_conn_id = 'datapg',
+        sql=
+        """
+                INSERT INTO currency(json, date) VALUES(
+                '{{ ti.xcom_pull(key='json_db', task_ids='transform_data') }}',
+                current_timestamp
+                );
+        """
+        )
+    extract >> transform >> create_table >> insert_in_table
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Currency
-This application shows the data of three currencies: KZT, RUB and BTC. Fresh data is added to the table every minute.
+This application shows the data of three currencies: KZT, RUB and BTC. Fresh data is added to the table every 20 minutes from Monday to Friday from 10:00 to 21:00.
 
 ## Launch
 You need Docker to run and PostgreSQL. <br>
@@ -7,6 +7,17 @@ You need Docker to run and PostgreSQL. <br>
 ```docker-compose up -d --build```
 2) In a short amount of time will be launched webserver, where will Airflow UI be. webserver is running at: <br>
 ```localhost:8080```
+3) In the UI, you need to go to: Admin -> Connections. Press "Create" and type:
+```
+Conn Id: datapg
+Conn Type: Postgres
+Host: postgres
+Schema: airflow
+Host: airflow
+Password: airflow
+Port: 5432
+Extra: clear
+```
 3) From the UI you can start the process. 
 4) To connect to a database: <br>
 ```
